77d63ee28b5eca01fa1ddd9b414359977d24e030
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-04 14:05:06 +0530

    Initial Commit 2

--- src/app/controllers/controllers.js
+++ src/app/controllers/controllers.js
@@ -1,31 +1,32 @@
 const { Url } = require('./../schemas/url.schema')
 
-const { regex } = require('./../utils/regex')
-
 const sendHomePage = async (req, res) => {
 	res.render("index.ejs");
 	res.end()
 }
-
+const regex = new RegExp(/^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:?#[\]@!\$&'\(\)\*\+,;=.]+$/gm)
 const handleInputData = async (req, res) => {
-	const url = req.body.url;
+	const url = await req.body.url;
+	const match = regex.test(url);
 	const checkForEntry = await Url.find({ original_url: url })
-	if (regex.test(url), checkForEntry.length === 0) {
+	if (match === true && checkForEntry.length === 0) {
+		console.log(checkForEntry.length)
 		let randomUrl = Math.floor(Math.random() * 100000)
 		const url = new Url({ original_url: req.body.url, short_url: randomUrl })
 		url.save().then(() => console.log("Saved To MDB"))
 		res.json({ original_url: req.body.url, short_url: randomUrl })
 	}
-	if (checkForEntry.length > 0) {
+	else if (checkForEntry.length > 0) {
+		console.log("Find Entry")
+		const findEntry = await Url.find({ original_url: url })
+		res.json({ original_url: findEntry[0].original_url, short_url: findEntry[0].short_url })
+	}
+	else {
+		console.log(regex.test(url))
 		if (!regex.test(url)) {
 			res.json({ error: "invalid url" })
 		}
-		else {
-			const findEntry = await Url.find({ original_url: url })
-			res.json({ original_url: findEntry[0].original_url, short_url: findEntry[0].short_url })
-		}
 	}
-	
 }
 
 const findById = async (req, res) => {
--- src/app/utils/regex.js
+++ src/app/utils/regex.js
@@ -1,3 +1,1 @@
 // const regex = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
-const regex = new RegExp(/^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm)
-module.exports = { regex }
